ab3f2d386123663c38cab7de5eefe99f026ba2f1
Kevin <vargaskevv@gmail.com>
2023-05-12 17:38:06 -0400

    basics empty project

---
+++ d_01_basics/bin/d_01_basics.dart
@@ -0,0 +1,5 @@
+import 'package:d_01_basics/d_01_basics.dart' as d_01_basics;
+
+void main(List<String> arguments) {
+  print('Hello world: ${d_01_basics.calculate()}!');
+}
---
+++ d_01_basics/test/d_01_basics_test.dart
@@ -0,0 +1,8 @@
+import 'package:d_01_basics/d_01_basics.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
